d9c835a3e9daa488a2079135c3f7f28cc1df3ed2
ehnvik <gustav.ehnvik@gmail.com>
2022-12-09 12:34:22 +0100

    Created a prducts file and added the first product

---
+++ src/ts/products/products.ts
@@ -0,0 +1,15 @@
+export const products = [
+  {
+    brand: "iphone",
+    name: "iphone 1",
+    description: "Silikonskal med MagSafe till iPhone 14 Pro Max",
+    price: 695,
+    amount: 1,
+    colors: ["red", "black", "blue"],
+    imageUrls: [
+      "/src/assets/img/iphone/iphone-1-red.jpg",
+      "/src/assets/img/iphone/iphone-1-black.jpg",
+      "/src/assets/img/iphone/iphone-1-blue.jpg",
+    ],
+  },
+];
