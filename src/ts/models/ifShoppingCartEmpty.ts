2b37d315a0a8dd3bcef11f38ed1cacbc278798cf
ehnvik <gustav.ehnvik@gmail.com>
2022-12-28 12:14:40 +0100

    Added amounts of products to shopping cart icon

--- src/ts/models/ifShoppingCartEmpty.ts
+++ src/ts/models/ifShoppingCartEmpty.ts
@@ -1,14 +1,43 @@
+import { CartItem } from "./CartItem";
+
 export function ifShoppingCartEmpty() {
+  let shoppingCartItemLink: HTMLAnchorElement = document.querySelector(
+    ".menu__bag-box__anchor"
+  ) as HTMLAnchorElement;
+
+  shoppingCartItemLink.classList.add("shopping-cart");
+
   let shoppingCartItem: HTMLAnchorElement = document.querySelector(
     ".menu__bag-box__anchor--logo"
   ) as HTMLAnchorElement;
+
+  let shoppingCartItemAmount: HTMLSpanElement = document.getElementById(
+    "amount-of-products-container"
+  ) as HTMLSpanElement;
+
+  shoppingCartItemLink.appendChild(shoppingCartItemAmount);
+
+  let cartItem: CartItem[] = JSON.parse(
+    localStorage.getItem("shoppingCart") || "[]"
+  );
 
   if (
     typeof localStorage["shoppingCart"] === "undefined" ||
     localStorage["shoppingCart"] === "[]"
   ) {
     shoppingCartItem.style.color = "white";
+    console.log("white");
   } else {
     shoppingCartItem.style.color = "#5c7e96";
+    shoppingCartItem.classList.add("shopping-cart__icon");
+    shoppingCartItemAmount.classList.add("shopping-cart__amount");
+    for (let i = 0; i < cartItem.length; i++) {
+      let numbersOfProductsInCart = cartItem.length;
+      let numbersOfProductsInCartText: string =
+        numbersOfProductsInCart.toString();
+      shoppingCartItemAmount.innerHTML = numbersOfProductsInCartText;
+    }
+
+    console.log("blue");
   }
 }
--- src/ts/products/itemsInShoppingCart.ts
+++ src/ts/products/itemsInShoppingCart.ts
@@ -1,14 +1,22 @@
-export function itemsInShoppingCart() {
-  let shoppingCartItem: HTMLAnchorElement = document.getElementById(
-    "menu__bag-box__anchor"
-  ) as HTMLAnchorElement;
+// import { CartItem } from "../models/CartItem";
 
-  if (
-    typeof localStorage["shoppingCart"] !== "undefined" ||
-    localStorage["shoppingCart"] !== "[]"
-  ) {
-    shoppingCartItem.style.color = "#5c7e96";
-  } else {
-    shoppingCartItem.style.color = "white";
-  }
-}
+// export function itemsInShoppingCart() {
+//   let shoppingCartItem: HTMLAnchorElement = document.getElementById(
+//     "shopping-cart-logo"
+//   ) as HTMLAnchorElement;
+
+//   let productAmount: number = 0;
+
+//   if (
+//     typeof localStorage["shoppingCart"] !== "undefined" ||
+//     localStorage["shoppingCart"] !== "[]"
+//   ) {
+//     shoppingCartItem.classList.add("shopping-cart-icon");
+//     shoppingCartItem.innerHTML = "1";
+//     for (let i = 0; i < cartItem.length; i++) {
+//       let numbersOfProductsInCart = cartItem[i];
+//     }
+//   } else {
+//     shoppingCartItem.style.color = "white";
+//   }
+// }
