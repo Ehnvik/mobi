f94693d5b0bdee5e31a26abb2719e1f028a0e44b
Tove Bosom <bboson@live.se>
2022-12-29 18:55:21 +0100

    added styling for checkout summary, created html for when order is placed. added total cost to summary

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -1,6 +1,5 @@
 import { CartItem } from "./models/CartItem";
 import { ifShoppingCartEmpty } from "./models/ifShoppingCartEmpty";
-import { getCartItemFromLs } from "./shoppingCart";
 
 window.addEventListener("load", () => {
   ifShoppingCartEmpty();
@@ -17,14 +16,14 @@ function lsFromCart() {
 
 lsFromCart();
 
-let checkoutContainer: HTMLDivElement = document.getElementById(
-  "checkout"
-) as HTMLDivElement;
-// let orderContainer: HTMLDivElement = document.getElementById(
-//   "orderSummaryContainer"
-// ) as HTMLDivElement;
-
 function summaryOfOrder(summary: CartItem[]) {
+  let totalCost: HTMLParagraphElement = document.getElementById(
+    "checkoutSummaryTotalCost"
+  ) as HTMLParagraphElement;
+
+  let arrayOfTotalCost: number[] = [];
+  let sum = 0;
+
   summary.forEach((summaryItem: CartItem) => {
     let orderContainer: HTMLDivElement = document.getElementById(
       "orderSummaryContainer"
@@ -35,26 +34,29 @@ function summaryOfOrder(summary: CartItem[]) {
       document.createElement("p");
     let summaryAmount: HTMLParagraphElement = document.createElement("p");
     let summaryPrice: HTMLParagraphElement = document.createElement("p");
-    let summaryTotal: HTMLHeadingElement = document.createElement("h4");
 
     summaryContainer.classList.add("summaryContainer");
     summaryDescription.classList.add("summaryContainer__description");
     summaryPrice.classList.add("summaryContainer__price");
     summaryAmount.classList.add("summaryContainer__amount");
     summaryColorContainer.classList.add("summaryContainer__color");
-    summaryTotal.classList.add("summaryContainer__total");
 
     summaryDescription.innerHTML = summaryItem.product.description;
     summaryColorContainer.innerHTML = summaryItem.color.toString();
-    summaryAmount.innerHTML = summaryItem.amount.toString();
+    summaryAmount.innerHTML = summaryItem.amount.toString() + " st";
     summaryPrice.innerHTML = summaryItem.product.price.toString() + " kr";
 
-    orderContainer.appendChild(summaryContainer);
+    arrayOfTotalCost.push(summaryItem.amount * summaryItem.product.price);
 
+    orderContainer.appendChild(summaryContainer);
     summaryContainer.appendChild(summaryDescription);
     summaryContainer.appendChild(summaryColorContainer);
     summaryContainer.appendChild(summaryAmount);
     summaryContainer.appendChild(summaryPrice);
-    summaryContainer.appendChild(summaryTotal);
   });
+  for (let i = 0; i < arrayOfTotalCost.length; i++) {
+    sum += arrayOfTotalCost[i];
+  }
+  let sumText: string = sum.toString();
+  totalCost.innerHTML = `Total summa: ${sumText} kr`;
 }
